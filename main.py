1ad044f54c0c00d896c0216773da1b7e98cc24ee
cloud-automation-marcel <80656229+cloud-automation-marcel@users.noreply.github.com>
2023-04-24 22:38:09 -0500

    Update main.py
    Add break between runs

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from datetime import datetime
 
 file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S.txt')
 with open('output_data.txt', 'a') as fd:
-    rows = [f"{file_name.replace('.txt','')}: {i}" 
+    rows = [f"\{file_name.replace('.txt','')}: {i}" 
             for i in range(10)]
-    rows = "\n".join(rows)
+    rows = "\n".join(rows) 
+    fd.write('\n')
     fd.write(rows)
