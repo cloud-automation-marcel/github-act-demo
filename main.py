a15a47fa2e76442c9d40e1ebe21652860bd662a5
cloud-automation-marcel <80656229+cloud-automation-marcel@users.noreply.github.com>
2023-04-24 22:18:09 -0500

    Changed file name
    Changed file name that removes the time.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S.txt')
-with open(file_name, 'w') as fd:
+with open('output_data.txt', 'a') as fd:
     rows = [f"{file_name.replace('.txt','')}: {i}" 
             for i in range(10)]
     rows = "\n".join(rows)
