a3f914cf6d680371b4ede7f0c42233167c2bf606
cloud-automation-marcel <80656229+cloud-automation-marcel@users.noreply.github.com>
2023-04-24 21:45:38 -0500

    Update main.py
    Add more lines into the file

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-for row in range(9):
-    print(row)
-  
+from datetime import datetime
+
+file_name = datetime.now().strftime('%Y-%m-%d_%H-%M-%S.txt')
+with open(file_name, 'w') as fd:
+    rows = [f"{file_name.replace('.txt','')}: {i}" 
+            for i in range(10)]
+    rows = "\n".join(rows)
+    fd.write(rows)
